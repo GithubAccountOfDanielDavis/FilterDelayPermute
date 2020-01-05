b82d426439e705fbf97236afb792b5e58ef84e27
GithubAccountOfDanielDavis <emailaddressofdanieldavis@gmail.com>
2020-01-05 01:28:49 -0500

    Added explicit backwards compatibility and error handling

--- fdp_numpy.py
+++ fdp_numpy.py
@@ -1,14 +1,16 @@
 from numpy import uint16, zeros, roll, frompyfunc, concatenate, object as obj
 from lookup_tables import PERMUTE, UNPERMUTE
 
-def encode (secret: bytes, delay_length=512, iterations=20):
-    assert delay_length > len(secret) + 1
+def encode (secret: bytes, delay_length=512, iterations=20, bw_compatible=True):
+    if not (delay_length > len(secret) + 1):
+        raise ValueError('delay length cannot be shorted than secret length')
 
     # Set up initial state and echoes block
     echoes = zeros(delay_length, dtype=uint16)
     if len(secret) > 0:
         echoes[-len(secret):] = list(secret)
-    echoes[0] = UNPERMUTE[echoes[0]] # backwards compatibility
+    if bw_compatible:
+        echoes[0] = UNPERMUTE[echoes[0]]
     state = 0
 
     # Create accumulator ufunc which generates a "running total"-like array
@@ -33,5 +35,6 @@ def encode (secret: bytes, delay_length=512, iterations=20):
         # "echo" entire block of states at once
         echoes = (interim_states >> 3) & 0xF00FF
 
-    echoes = roll(echoes, len(secret)) # backwards compatibility
+    if bw_compatible:
+        echoes = roll(echoes, len(secret))
     return state, echoes
